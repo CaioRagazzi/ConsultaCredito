de7454b728fb836db5eba2fed757b4da30e9f8ea
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2018-10-10 13:04:42 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,9 +2,9 @@ pipeline {
     agent any
 
     stages {
-        stage('Restore') {
+        stage('Build') {
             steps {
-                echo 'Restoring..'
+                echo 'Building..'
                 bat "dotnet build"
             }
         }
