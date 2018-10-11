62143e035b2b2fe8200db60c29de679afe77ce93
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2018-10-11 16:12:03 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,6 +2,15 @@ pipeline {
     agent any
 
     stages {
+         stage('Load Parameters') {
+            steps {
+                script{
+                    println = "=======Load Parameters"
+                    checkout ([$class: 'GitSCM', userRemoteConfigs: [[url: https://github.com/CaioRagazzi/ConsultaCredito/edit/master/Jenkinsfile.groovy.git, credentialsId: credential]], branches: [[name: tag-version]]],poll: false)
+                }
+                
+            }
+        }
         stage('Restore') {
             steps {
                 echo 'Restoring...'
