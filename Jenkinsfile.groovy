22cb62aa6f80c240391d9bcbbf9f5701ae2c8302
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2018-10-11 17:19:50 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,7 +6,7 @@ pipeline {
             steps {
                 script{
                     println "=======Load Parameters============"
-                    
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/CaioRagazzi/ConsultaCredito.git']]])
                 }
             }
         }
