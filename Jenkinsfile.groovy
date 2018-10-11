b9bdcae1b1beef240e2ee7439e3195034b42abc6
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2018-10-11 10:40:02 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building..'
-                bat "dotnet build"
+                bat "dotnet clean"
             }
         }
         stage('Deploy') {
